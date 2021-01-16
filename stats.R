59fdc07f37ce2328f3d052e6c4f78ad8d34ab630
Peter Zhang <petejzh@gmail.com>
2021-01-15 22:03:51 -0500

    new data

--- stats.R
+++ stats.R
@@ -7,25 +7,21 @@ income19 <- as.numeric(scan("datasets/incomes19.txt", what="", sep="\n"))
 income20 <- as.numeric(scan("datasets/incomes20.txt", what="", sep="\n"))
 
 # test for normality
-shapiro.test(acts19)
 ggplot(data.frame(act=acts19), aes(x=act)) + geom_histogram()
-shapiro.test(acts20)
 ggplot(data.frame(act=acts20), aes(x=act)) + geom_histogram()
 
 # significance test for act
-se1 = sd(data19)/sqrt(length(data19))
-se2 = sd(data20)/sqrt(length(data20))
+se1 = sd(acts19)/sqrt(length(acts19))
+se2 = sd(acts20)/sqrt(length(acts20))
 se_pooled = sqrt(se1^2 + se2^2)
-1-pnorm(0.079/se_pooled)
+1-pnorm((mean(acts20)-mean(acts19))/se_pooled)
 
 # test for normality
-shapiro.test(income19)
 ggplot(data.frame(income=income19), aes(x=income)) + geom_histogram()
-shapiro.test(income20)
 ggplot(data.frame(income=income20), aes(x=income)) + geom_histogram()
 
 # significance test for income
-se1 = sd(data19)/sqrt(length(data19))
-se2 = sd(data20)/sqrt(length(data20))
+se1 = sd(income19)/sqrt(length(income19))
+se2 = sd(income20)/sqrt(length(income20))
 se_pooled = sqrt(se1^2 + se2^2)
-1-pnorm(811/se_pooled)
+1-pnorm((mean(income20)-mean(income19))/se_pooled)
